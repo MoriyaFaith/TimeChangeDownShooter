507c7edcdc5af36acb0c12a7ce2f3c398c2cef70
Jack H Ramirez <jramirez14@fingerlakes.edu>
2022-04-18 10:55:30 -0400

    Keys and Doors

---
+++ Assets/Scripts/HealthSystem - Copy.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] private int maxHp = 3;
+    [SerializeField] private int hp = 3;
+
+    [SerializeField] private UnityEvent OnDamaged;
+    [SerializeField] private UnityEvent OnZero;
+    [SerializeField] private UnityEvent XDeathState; //Gibs
+    
+    
+    public void Damage(int hpAmount)
+    {
+        hp -= hpAmount;
+        Debug.Log("hp amount changed by " + hpAmount + " and is now " + hp);
+        
+        OnDamaged?.Invoke();
+
+        if (hp <= -maxHp) //gibs?
+        {
+            XDeathState?.Invoke();
+        }
+        else if (hp <= 0)
+        {
+            OnZero?.Invoke();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README - Copy.md
@@ -0,0 +1,2 @@
+# topdownshootergame
+ 
