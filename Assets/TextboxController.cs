2b60249bee058264e42b81ac82562e03772608cd
MoriyaFaith <46070717+MoriyaFaith@users.noreply.github.com>
2022-04-20 10:33:27 -0400

    Began work on Text

---
+++ Assets/TextboxController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextboxController : MonoBehaviour
+{
+    private TMP_Text _text;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _text = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _text.text = "BRUH";
+    }
+    
+    public void UpdateText(System.String content)
+    {
+        _text.text = content;
+    }
+    
+}
